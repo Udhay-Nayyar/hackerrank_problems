e81738d67494079a599197a23ec13299853dc062
Udhay-Nayyar <udhaynayyar80@gmail.com>
2025-02-10 21:43:03 +0530

    Add files via upload

---
+++ Swap-two-no.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("a = %d\n",b);
+    printf("b = %d",a);
+    return 0;
+}
