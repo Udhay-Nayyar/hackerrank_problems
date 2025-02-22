c5e25ebe758dfaf4b4e1b346f359bcac60807c47
UDHAY NAYYAR <udhaynayyar80@gmail.com>
2025-02-22 23:21:33 +0530

    Add files via upload

---
+++ Bitwise_Even or Odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    if (N & 1 == 1)
+    {
+        printf("Odd");
+    }
+    else
+    {
+        printf("Even");
+    }
+    return 0;
+}
